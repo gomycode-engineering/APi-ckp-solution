1b0be50538f0e61311d4cf4082ff5579612765e9
Anis Melliti <ansmeliti@gmail.com>
2020-06-05 23:57:32 +0100

    implement the solution

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+import Users from './Components/Users.js/Users';
+const App = () => {
+  return (
+    <div className='container-fluid'>
+      <Users />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Users.js/Users.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import User from '../User/User';
+
+const Users = () => {
+  const [isLoading, setisLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setisLoading(true);
+    const getData = async () => {
+      try {
+        const usersArray = await axios.get(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        setUsers(usersArray.data);
+        setisLoading(false);
+      } catch (error) {
+        setUsers([]);
+      }
+    };
+    getData();
+    return () => {};
+  }, []);
+
+  return isLoading ? (
+    <div className='row justify-content-md-center'>
+      <div className='spinner-border' role='status'>
+        <span className='sr-only'>Loading...</span>
+      </div>
+    </div>
+  ) : (
+    <div className='row'>
+      {users.map((user, key) => (
+        <User user={user} key={key} />
+      ))}
+    </div>
+  );
+};
+
+export default Users;
